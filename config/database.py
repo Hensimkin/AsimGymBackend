62a0eea52f4ef7e3e8c8bac7131def69b56276b9
Hen Simkin <hensi1@ac.sce.ac.il>
2024-09-10 13:34:38 +0300

    forgot password was added

--- config/database.py
+++ config/database.py
@@ -3,13 +3,16 @@ from pymongo import MongoClient
 
 uri = "mongodb+srv://hensim97:I6yrc4wEom8ree74@asimgym.hdubeoh.mongodb.net/?retryWrites=true&w=majority&appName=AsimGym"
 client = MongoClient(uri,ssl_ca_certs=certifi.where())
+
 db=client["AsimGym"]
 
 userCollection=db["users"]
-# Reference the userConfiguration collection
+
 userConfigurationCollection = db["userConfiguration"]
 
 customExercisesCollection=db["customExercises"]
+
+usersExercisesLogCollection=db["usersExercisesLog"]
 
 try:
     client.admin.command('ping')
--- routes/route.py
+++ routes/route.py
@@ -9,12 +9,14 @@ import httpx
 from config.database import userCollection
 from config.database import userConfigurationCollection
 from config.database import customExercisesCollection
+from config.database import usersExercisesLogCollection
 from models.users import User
 from models.userLogin import UserLogin
 from models.emails import Email
 from models.line import UpdatedExcersice
 from models.userConfiguration import UserData
 from models.savedExercise import CustomExcersice
+from models.userExcersice import UserExcersice
 from datetime import datetime, timedelta
 import jwt
 from passlib.context import CryptContext
@@ -31,6 +33,8 @@ logger = logging.getLogger(__name__)# printing
 from typing import List
 from models.Exercise import Exercise
 from models.ExercisesResponse import ExercisesResponse
+
+from typing import List, Dict, Any
 
 
 SECRET_KEY = "784255b18b16ad873a7048bd745428647b3697ea3c0250e7089a69fc9ed7a268"
@@ -307,6 +311,16 @@ async def verify_user_Configuration(userEmail: Email):
 @router.post("/api/user/saveExercises") #creating custom excercise with 0 0 0
 async def createCustomExcersice(newExcersice:CustomExcersice):
     try:
+        existing_exercise = customExercisesCollection.find_one({
+            "name": newExcersice.name,
+            "userEmail": newExcersice.userEmail
+        })
+
+        print(existing_exercise)
+
+        if existing_exercise:
+            return {"message": "ChangeName"}
+
         # Transform the input data into the desired format
         exercises_data = {}
         for exercise in newExcersice.exercises:
@@ -341,7 +355,7 @@ async def updateCustomExcersice(updatedExcercise:UpdatedExcersice):
                 {"$set": {f"exercises.{exercise_name}": details}},
                 upsert=True
             )
-
+        print(updatedExcercise.excersicename)
         return {"message": "Exercises updated successfully"}
 
     except Exception as e:
@@ -364,6 +378,123 @@ async def getExcerciesNames(email:Email):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@router.post("/api/user/getExerciseProgram")  # Corrected the typo in the endpoint
+async def getExerciseProgram(userExercise: UserExcersice):
+    try:
+        print(userExercise.excersicename)
+        user_exercises = customExercisesCollection.find({"userEmail": userExercise.email,"name":userExercise.excersicename})
+
+        exercise_details = []
+        for exercise_doc in user_exercises:
+            for exercise_name, exercise_info in exercise_doc['exercises'].items():
+                exercise_details.append({
+                    "exercise_name": exercise_name,
+                    "reps": exercise_info.get("reps"),
+                    "sets": exercise_info.get("sets"),
+                    "weight": exercise_info.get("weight")
+                })
+
+        print(exercise_details)
+        return {"exerciseProgram": exercise_details}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/api/user/updateProfile")
+async def updateProfile(updateDetails: UserData):
+    try:
+        userdetails = userConfigurationCollection.find_one({"email": updateDetails.email})
+        if userdetails:
+            # Creating the update dictionary excluding email
+            update_data = {
+                "age": updateDetails.age,
+                "fitnessLevel": updateDetails.fitnessLevel,
+                "gender": updateDetails.gender,
+                "goal": updateDetails.goal,
+                "height": updateDetails.height,
+                "selectedMuscles": updateDetails.selectedMuscles,
+                "weight": updateDetails.weight
+            }
+
+            userConfigurationCollection.update_one(
+                {"email": updateDetails.email},
+                {"$set": update_data}
+            )
+            return {"message": "Profile updated successfully"}
+        else:
+            return {"message": "User not found"}
+    except Exception as e:
+        return {"message": f"An error occurred: {str(e)}"}
 
 
 
+@router.get("/api/user/getProfile")
+async def getProfile(email: str = Query(..., alias='email')):
+    try:
+        userdetails = userConfigurationCollection.find_one({"email": email})
+        if userdetails:
+            # Remove the ObjectId from the result to make it serializable
+            userdetails['_id'] = str(userdetails['_id'])
+            return {"userdetails": userdetails}
+        else:
+            return {"message": "User not found"}
+    except Exception as e:
+        return {"message": f"An error occurred: {str(e)}"}
+
+
+@router.post("/api/user/postExerciseLog")
+async def post_exercise_log(exercise_log_str: str = Body(...)):
+    try:
+        # Parse the incoming string into a dictionary
+        exercise_log_dict = json.loads(exercise_log_str)
+
+
+
+        # Transform the exercises array into an object
+        exercises_transformed = {}
+        for exercise in exercise_log_dict["exercises"]:
+            exercise_name = exercise.pop("name")
+            exercises_transformed[exercise_name] = exercise
+
+        # Replace the exercises list with the transformed object
+        exercise_log_dict["exercises"] = exercises_transformed
+
+
+        # Insert the exercise log into the database
+        usersExercisesLogCollection.insert_one(exercise_log_dict)
+
+        # Return a success message with the inserted ID
+        return {"status": "success", "message": "Data saved successfully."}
+
+    except Exception as e:
+        # Handle any error that occurs
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+def serialize_log(log: Dict[str, Any]) -> Dict[str, Any]:
+    # Remove the _id field and return the rest of the log
+    log.pop('_id', None)  # Remove _id if it exists
+    return log
+@router.post("/api/user/getExerciseLog")
+async def get_exercise_log(email: Email):
+    logs = list(usersExercisesLogCollection.find({"userEmail": email.email}))
+    serialized_logs = [serialize_log(log) for log in logs]
+
+    return {"logs": serialized_logs}
+
+
+@router.post("/api/user/forgotPassword")
+async def forgotPassword(user:UserLogin):
+    db_user = userCollection.find_one({"email": user.email})
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    hashed_password = pwd_context.hash(user.password)
+    result = userCollection.update_one(
+        {"email": user.email},
+        {"$set": {"password": hashed_password}}
+    )
+
+    if result.modified_count == 1:
+        return {"message": "Password updated successfully"}
+    else:
+        raise HTTPException(status_code=500, detail="Password update failed")
