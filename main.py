def18632f3271a86145648287413af7686591b3f
Hen Simkin <hensi1@ac.sce.ac.il>
2024-09-14 19:35:33 +0300

    main

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from typing import Union
 
+# import uvicorn
+#
+# from routes.route import router as user_router
+#
+# from fastapi import FastAPI
+#
+#
+# app = FastAPI()
+#
+# app.include_router(user_router)
+#
+#
+#
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="localhost", port=8000)
+
+
 import uvicorn
-
-from routes.route import router as user_router
-
 from fastapi import FastAPI
-
+from routes.route import router as user_router
 
 app = FastAPI()
 
 app.include_router(user_router)
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
